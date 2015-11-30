0d0f77c8277ef2fd07dd396eb3bd4ec252ac144f
mickedav <mikda221@student.liu.se>
2015-11-30 12:18:59 +0100

    Made looping over days possible
    ATM working for certain day of week for entire month

--- setCellSpeedDay.m
+++ setCellSpeedDay.m
@@ -2,17 +2,15 @@ function [cellSpeed2, endSec] = setCellSpeedDay(intData, doubleData, timeStampDa
 import netconfig.*
 
 [numberOfCells, cellSize, lengthStretch, totalNumberOfCells] = getCellMap(network, linkIdArray);
-
 startSec = min(timeStampData(:,1));
-%endSec = max(timeStampData(:,2));
-endSec = 7260;
-
-K = NaN(row,50,endSec);
+endSec = 7200 + 3600;
+clear K
+K = NaN(row,totalNumberOfCells,endSec);
 hej = 1;
 
 for i = 1:row
     A = NaN(1,endSec);
-    
+    timeStampData(i,2)
     a = Spot(network.getLinkWithID(intData(i,1)), doubleData(i,1), -1);
     b = Spot(network.getLinkWithID(intData(i,2)), doubleData(i,2), -1);
     
@@ -24,8 +22,7 @@ for i = 1:row
         
         startCell = getCellId(a, linkIdArray, numberOfCells, cellSize);
         endCell = getCellId(b, linkIdArray, numberOfCells, cellSize);
-        
-        kaka = setCellSpeedTaxi(startCell, endCell, v, totalNumberOfCells);
+        getTaxiSpeed = setCellSpeedTaxi(startCell, endCell, v, totalNumberOfCells);
 
         A(1,timeStampData(i,1):timeStampData(i,2))= v;
     catch
@@ -33,27 +30,24 @@ for i = 1:row
     end
     
     
-    for apa = 1:50
-        if ~isnan(kaka(apa))
-            hej = hej + 1;
-            K(i,apa,:) = A;
+    for k = 1:totalNumberOfCells
+        if ~isnan(getTaxiSpeed(k))
+            K(i,k,:) = A;
         end
     end
 end
 k = K;
 
 for i = 1:endSec
-    for j = 1:50
+    for j = 1:totalNumberOfCells
     	cellSpeed(i,j) = nanmean(k(:,j,i));
     end
 end
 for i = 60:60:endSec - 60
-    i/60
-    for j = 1:50
+    for j = 1:totalNumberOfCells
         cellSpeed2(i/60, j) = nanmean(cellSpeed(i:i+59,j));
     end
 end
-size(cellSpeed(:,1))
 cellSpeed2 = cellSpeed2';
 
 end
