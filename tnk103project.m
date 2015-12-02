271d1b21b1eb7afe21e2228405eaee83c774fc8c
valeriedahl <valda355@student.liu.se>
2015-12-02 16:56:53 +0100

    developing algorithm 2

--- tnk103project.m
+++ tnk103project.m
@@ -106,13 +106,19 @@ end
 % plotHeatMap(sensorAllCellsSpeedArray.*3.6,startTime, endTime, numberOfTimeSteps);
 %%
 
-%% algoritm 2 -
-% fill lengthFromStart with the length from start to half of the cell i:s
-% length
-lengthFromStart = zeros(totalNumberOfCells);
-lengthFromStart(1)=cellSize(1)/2;
+%% START OF ALGORTIHM 2
+% fill lengthFromStartHalf with the length from start to half of the cell
+% for each cell
+% fill lengthFromStartReal with the length from start to the end of the
+% cell for each cell
 
-for i=1:totalNumberOfCells
+% indexSensorArray consists of each sensor's cell number
+sensorCellSpeedArray(isnan(sensorCellSpeedArray)) = 0;
+indexSensorArray = find(sensorCellSpeedArray(:,1));
+
+lengthFromStartHalf(1) = cellSize(1)/2;
+lengthFromStartReal(1) = cellSize(1);
+for i=2:totalNumberOfCells
     currentNumberOfCells = 0;
     index = 0;
     for j=1:numberOfLinks
@@ -120,12 +126,68 @@ for i=1:totalNumberOfCells
         index = index + 1;
         %         break if cell t is on link number index
         if (i/currentNumberOfCells) <=1
-            lengthFromStart(i) = sum(lengthFromStart(:))+lengthFromStart(end) + cellSize(index)./2;
+            lengthFromStartHalf(i) = lengthFromStartReal(end) + cellSize(index)./2;
+            lengthFromStartReal(i) = lengthFromStartReal(end) + cellSize(index);
             break;
         end
     end
 end
+
+% fill lengthBetweenSensors with the distance (meters) between two sensors
+for i=2:(numberOfSensors-1)
+    distanceBetweenSensors(i-1) = lengthFromStartHalf(indexSensorArray(i))-lengthFromStartHalf(indexSensorArray(i-1));
+end
+
+% average distance between two sensors
+averageDistanceSensor = mean(distanceBetweenSensors);
+
 %%
+% sigma is calculated as half of the average distance between two sensors
+sigma = averageDistanceSensor/2;
+% tau is set to half of the aggregated interval (1 min)
+tau = 0.5;
+%%
+
+% %  ------------ FORTSÄTT HÄR -----------------
+% for t=numberOfTimeSteps
+    
+    % loop through all the sensors
+    for i=2:(numberOfSensors-1)
+        sensor1 = indexSensorArray(i-1);
+        sensor2 = indexSensorArray(i);
+        
+        % if the two sensors are not in neighboring cells, no estimation will be
+        % done
+        if sensor1+1 ~= sensor2
+            
+            % for the first cell after the first sensor to the last cell before the
+            % next sensor, t.ex. cell 10-11
+            for cell=(sensor1+1):(sensor2-1)
+                
+                % loop for two sensors at the time
+                % x is the position in the middle on the cell we want to estimate the speed
+                % in
+                x = lengthFromStartHalf(cell);
+                x1 = lengthFromStartHalf(sensor1);
+                x2 = lengthFromStartHalf(sensor2);
+                t = 2;
+                t1 = t-1;
+                t2 = t+1;
+                
+                N(cell,2) = exp(-((abs(x-x1)/sigma)+(abs(t-t1)/tau))) + exp(-((abs(x-x2)/sigma)+(abs(t-t2)/tau)));
+                sumNv(cell,2) = exp(-((abs(x-x1)/sigma)+(abs(t-t1)/tau)))*sensorCellSpeedArray(sensor1,t1) + exp(-((abs(x-x2)/sigma)+(abs(t-t2)/tau)))*sensorCellSpeedArray(sensor2,t2);
+                
+            end
+        end
+        sumNv(cell,2)/N(cell,2)
+%            estimatedSpeed = sensorCellSpeedArray;
+%            estimatedSpeed 
+        
+    end
+% end
+
+
+%% END OF ALGORTIHM 2
 
 %% difference between arrays
 %     sensorCellSpeedArrayDay(:,:,day);
