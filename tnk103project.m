c490161445df0423595e39089ee4e5acb6879e8c
valeriedahl <valda355@student.liu.se>
2015-12-09 14:11:28 +0100

    Update tnk103project.m

--- tnk103project.m
+++ tnk103project.m
@@ -83,7 +83,7 @@ for day = 1:numberOfDays
         %% create the
         [sensorCellSpeedArray, sensorCellTravelTimesArray,indexArray]=setCellDataSensor(numberOfCells,network,sensorIdArray,totalNumberOfCells,numberOfTimeSteps,numberOfSensors,linkIdArray,cellSize,sensorSpeedArray);
         %%
-        % spara för varje vecka
+        % spara fÃ¶r varje vecka
         sensorCellSpeedArrayWeek(:,:,week) = sensorCellSpeedArray;
         
         
@@ -125,12 +125,160 @@ estimatedSpeedAlg2 = algorithm2(sensorCellSpeedArray,cellSize,numberOfTimeSteps,
 % time for the stretch between cell 9-50.
 [hej,travelTimesArray] = travelTimesInterval(estimatedSpeedAlg2, steplength, cellSizeAll, numberOfTimeSteps);
 
+
+
+%%
+% % sigma is calculated as half of the average distance between two sensors
+% sigma = averageDistanceSensor/2;
+% tau is set to half of the aggregated interval (1 min)
+tau = 0.5;
+%%
+
+% cellSpeedAggregatedTime contains NaN-values and GPSCellSpeedArray
+% contains zeros instead of NaN
+GPSCellSpeedArray = cellSpeedAggregatedTime;
+GPSCellSpeedArray(isnan(GPSCellSpeedArray)) = 0;
+
+% Loop through all timesteps
+for t=2:(numberOfTimeSteps-1)
+    t
+    % (numberOfTimeSteps-1)
+   
+    % Loop through all cells from cell 9
+    for cell=firstCell:(totalNumberOfCells-1)
+         
+        if isnan(cellSpeedAggregatedTime(cell,t))
+%             y1 = 1; y2 = 1;
+noMeasure1 = 1; noMeasure2 = 1; tooLong1 = 1; tooLong2 = 1; 
+            % x is the position in the middle on the cell we want to estimate the speed
+            % in
+            x = lengthFromStartHalf(cell);
+            t1 = t+1;
+            t2 = t-1;
+            
+            % loop for finding x1
+            for i=(cell-1):-1:1
+                
+                if GPSCellSpeedArray(i,t1)== 0 && isnan(cellSpeedAggregatedTime(i,t1))
+                    %  if there are no measurement in the cell -> do not use
+                    %  any value
+                    %  GPSCellSpeedArray(i,t1)=0;
+                    x1 = 0;
+                    noMeasure1 = 0;
+                    cellGPS1 = i;
+                    
+                else
+                    %   if there is a measured or estimated speed that is greater than zero ->
+                    %   use this value
+                    % x1 is the distance to the closest prevois data point
+                    % cellGPS1 is the cell number where the closest prevois data point is
+                    % located
+                    x1 = lengthFromStartHalf(i);
+                    cellGPS1 = i;
+                    if abs(x-x1) > 1000
+                        % if the distance between the data points is
+                        % larger than 1 km -> do not use any value
+                        % GPSCellSpeedArray(i,t1)=0;
+                        tooLong1 = 0;
+                    end
+                    
+                    break;
+                    
+                end
+                
+            end
+            
+            
+            % loop for finding x2
+            for i=(cell+1):totalNumberOfCells
+                
+                if GPSCellSpeedArray(i,t2)== 0 && isnan(cellSpeedAggregatedTime(i,t2))
+                    %  if there are no measurement in the cell -> do not use
+                    %  any value
+                    %                     GPSCellSpeedArray(i,t2)=0;
+                    x2 = 0;
+                    noMeasure2 = 0;
+                    cellGPS2 = i;
+                    
+                else
+                    %   if there is a measured or estimated speed that is greater than zero ->
+                    %   use this value
+                    % x2 is the distance to the closest prevois data point
+                    % cellGPS2 is the cell number where the closest prevois data point is
+                    % located
+                    x2 = lengthFromStartHalf(i);
+                    cellGPS2 = i;
+                    
+                    if abs(x-x2) > 1000
+                        % if the distance between the data points is
+                        % larger than 1 km -> do not use any value
+                        %                          GPSCellSpeedArray(i,t2)=0;
+                        tooLong2 = 0;
+                    end
+                    
+                    break;
+                    
+                end
+                
+            end
+            
+            % sigma is calculated as half of the distance between the two data points
+            % are used to estimate the speed in the cell
+            if noMeasure1 == 0 || tooLong1 == 0
+                sigma = abs(x-x2)/2;
+%                 y1 = 0;
+            elseif  noMeasure2 == 0 || tooLong2 == 0
+                sigma = abs(x-x1)/2;
+%                 y2 = 0;
+            else
+                sigma = abs(x1-x2)/2;
+            end
+            
+            % if the measurements are too far away (> 1 km) or no
+            % measurements in the neigboring time periods -> set the speed
+            % to the closest measurement/estimated speed in the same time
+            % period
+            if (noMeasure1 == 0 || tooLong1 ==0) && (noMeasure2 == 0 || tooLong2 == 0)
+                if noMeasure1 == 0 
+               GPSCellSpeedArray(cell,t)=GPSCellSpeedArray(cell+1,t);
+                elseif noMeasure2 == 0 
+                        GPSCellSpeedArray(cell,t)=GPSCellSpeedArray(cell-1,t);
+                end
+            else
+                
+                y1 = noMeasure1*tooLong1;
+                y2 = noMeasure2*tooLong2;
+                
+                N = y1*exp(-((abs(x-x1)/sigma)+(abs(t-t1)/tau))) + y2*exp(-((abs(x-x2)/sigma)+(abs(t-t2)/tau)));
+                sumNv = y1*exp(-((abs(x-x1)/sigma)+(abs(t-t1)/tau)))*GPSCellSpeedArray(cellGPS1,t1) + y2*exp(-((abs(x-x2)/sigma)+(abs(t-t2)/tau)))*GPSCellSpeedArray(cellGPS2,t2);
+                GPSCellSpeedArray(cell,t)=sumNv/N;
+            end
+            
+            
+            %           % loop for two sensors at the time
+            
+            %                 x1 = lengthFromStartHalf(sensor1);
+            %                 x2 = lengthFromStartHalf(sensor2);
+            %                 t2 = t-1;
+            %                 t1 = t+1;
+            
+        end
+        
+        
+    end
+    
+end
+ plotHeatMap(GPSCellSpeedArray,startTime, endTime, numberOfTimeSteps, 'Algorithm 4: GPS data only');
+
+%%
+
 figure(3)
 plot(hej')
 % plot the travelTimes at different start times
 figure(4)
 plotTravelTimesDifferentStartTimes(travelTimesArray,startTime,endTime, steplength);
 % travelTimesArray'
+
 
 %% algorithm 3 - radar sensors, adaptive smoothing method
 % h=figure(3)
@@ -167,21 +315,21 @@ plotTravelTimesDifferentStartTimes(travelTimesArray,startTime,endTime, steplengt
 %%
 
 %% Spara ny colomap: %%
-% 1. Kör följande i m-fil.
+% 1. KÃ¶r fÃ¶ljande i m-fil.
 %   colormap ('jet')
 %   title('speed contour plot')
 %   imagesc(temp);
 %   colorbar;
-% 2. Öppna colormapeditor från kommandofönstret och ändra till önskad
+% 2. Ã–ppna colormapeditor frÃ¥n kommandofÃ¶nstret och Ã¤ndra till Ã¶nskad
 % layout
-% 3. Spara layoten i egen variabel från kommandofönstret:
+% 3. Spara layoten i egen variabel frÃ¥n kommandofÃ¶nstret:
 % cm=colormap
 % 4. Spara ner cm i en mat-fil
 % save mycmap cm
 %
 %%
 
-%% Sparas endast för om vi vill använda cell arrays
+%% Sparas endast fÃ¶r om vi vill anvÃ¤nda cell arrays
 % % sensorDataCellArray = cell(size(linkIdArray),1)
 %
 % % Initialize cellSpeed which consists of the speed for each cell in each
